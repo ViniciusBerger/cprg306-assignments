ba47a5d6351d5acb316e8ae2e0d6452b5b0c9c7d
Marcos Vinicius Berger Gilles <145512237+ViniciusBerger@users.noreply.github.com>
2025-03-26 10:40:21 -0600

    error week-9

--- app/week-9/layout.js
+++ app/week-9/layout.js
@@ -1,5 +1,5 @@
 import { AuthContextProvider } from "./_utils/auth-context";
- 
+
 export default function Layout({ children }) {
   return <AuthContextProvider>{children}</AuthContextProvider>;
 }
--- app/week-9/page.js
+++ app/week-9/page.js
@@ -1,23 +1,50 @@
-"use client"
+"use client";
 
+import Link from "next/link";
 // Import the useUserAuth hook
 import { useUserAuth } from "./_utils/auth-context";
- 
-export default async function Page(){
-    // Use the useUserAuth hook to get the user object and the login and logout functions
-    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
- 
-    // Sign in to Firebase with GitHub authentication
-    await gitHubSignIn();
- 
-    // Sign out of Firebase
-    await firebaseSignOut();
- 
-    return (
-        <div>
-            // Display some of the user's information
-            <p>
-            Welcome, {user.displayName} ({user.email})
-            </p>;
-        </div>
-)}
+
+export default function Page() {
+  // Use the useUserAuth hook to get the user object and the login and logout functions
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <div>
+        {user ? (
+          <button
+            onClick={firebaseSignOut}
+            className="bg-red-400 text-black px-4 py-2 rounded-md hover:bg-red-600 transition"
+          >
+            Sign out
+          </button>
+        ) : (
+          <button
+            onClick={gitHubSignIn}
+            className="bg-gray-800 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition"
+          >
+            Sign in with GitHub
+          </button>
+        )}
+      </div>
+      <div>
+        {user && (
+          <div className="mt-6 bg-white shadow-lg rounded-lg p-6 w-full max-w-md text-center">
+            <h2 className="font-bold text-2xl m-2 p-2">
+              It's good to see you again {user.displayName}!
+            </h2>
+            <p>E-mail: {user.email}</p>
+            <p className="m-2 p-2">
+              <Link
+                href="/week-9/shopping-list"
+                className="text-blue-500 hover:underline"
+              >
+                Access Shopping List
+              </Link>
+            </p>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}
